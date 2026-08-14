f79efa07af56ac137b0a2ddfeaf8f46ef68d54e1
erstazi <erstazi@gmail.com>
2026-08-14 00:43:18 -0400

    Testing cottage's anvil cottages_anvil = false

--- init.lua
+++ init.lua
@@ -113,6 +113,8 @@ if minetest.settings:get_bool("cottages_hay", true) then
 	dofile(minetest.get_modpath("cottages").."/nodes_hay.lua");
 end
 
+local anvil_enabled = minetest.settings:get_bool("cottages_anvil", true)
+minetest.log("warning", "COTTAGES_ANVIL = " .. tostring(anvil_enabled)
 -- if minetest.settings:get_bool("cottages_anvil", true) then
 	-- dofile(minetest.get_modpath("cottages").."/nodes_anvil.lua");
 -- end
